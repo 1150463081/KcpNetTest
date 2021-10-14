df780b88aa2b4eab71e7738973461ac7fcdfb911
1150463081 <63703895+1150463081@users.noreply.github.com>
2021-10-14 14:35:48 +0800

    Update:首次提交

---
+++ KCPNet/KCPHandle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets.Kcp;
+using System.Buffers;
+
+namespace KCPNet
+{
+    /// <summary>
+    /// kcp数据处理器
+    /// </summary>
+    public class KCPHandle : IKcpCallback
+    {
+        public Action<Memory<byte>> Out;
+        public void Output(IMemoryOwner<byte> buffer, int avalidLength)
+        {
+            using (buffer)
+                Out(buffer.Memory.Slice(0, avalidLength));
+        }
+
+        public Action<byte[]> Recv;
+        public void Recive(byte[] buffer)
+        {
+            Recv(buffer);
+        }
+    }
+}
